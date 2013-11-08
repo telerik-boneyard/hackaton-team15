576063cf551f6b95b40d09c50f38cae259d2f767
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2013-11-08 09:41:27 -0600

    some styling stuff icons etc

--- Eventer/scripts/app/main.js
+++ Eventer/scripts/app/main.js
@@ -77,10 +77,11 @@ var app = (function () {
         },
         formatDate: function (dateString) {
             var date = new Date(dateString);
-            var year = date.getFullYear().toString();
-            var month = date.getMonth().toString();
-            var day = date.getDate().toString();
-            return day + '.' + month + '.' + year;
+            //var year = date.getFullYear().toString();
+            //var month = date.getMonth().toString();
+            //var day = date.getDate().toString();
+            var minutes = date.getMinutes() === 0 ? "00" : date.getMinutes();
+            return date.getHours() + ':' + minutes;
         },
         logout: function () {
             return el.Users.logout();
